9712f2b8046a933e0e680361295bcc7513db069e
flavio <flavio.bordoni.dev@gmail.com>
2022-10-08 21:32:42 +0200

    Mockups update

--- app/controllers/mockups_controller.rb
+++ app/controllers/mockups_controller.rb
@@ -1,5 +1,36 @@
 class MockupsController < ApplicationController
-  def index
+  def page_a
+  end
+
+  def edu_sign_in
+    render layout: 'edu_demo'
+  end
+
+  def edu_index
+    render layout: 'edu_demo'
+  end
+
+  def edu_student_bookmark
+    render layout: 'edu_demo'
+  end
+
+  def edu_faq
+    render layout: 'edu_demo'
+  end
+
+  def sign_in
+    render layout: 'edu_demo'
+  end
+
+  def homepage
+    render layout: 'edu_demo'
+  end
+
+  def lessons_show
+    render layout: 'edu_demo'
+  end
+
+  def lessons_show_steps_show
     render layout: 'edu_demo'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
-  get 'mockups/index'
+  get 'mockups/page_a'
+  get 'mockups/edu_sign_in'
+  get 'mockups/edu_index'
+  get 'mockups/edu_student_bookmark'
+  get 'mockups/edu_faq'
+  get 'mockups/sign_in'
+  get 'mockups/homepage'
+  get 'mockups/lessons_show'
+  get 'mockups/lessons_show_steps_show'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root 'mockups#index'
+  root 'mockups#edu_index'
 end
