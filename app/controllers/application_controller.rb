ccab35595ec6d487ca9e4cb0c04b1b71073d8233
flavio <flavio.bordoni.dev@gmail.com>
2022-10-26 01:53:03 +0200

    set locale via url

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+
+  #-----------------------------------------------------------------------------
+  private
+
+    #keep the locale through links
+    def default_url_options(options = {})
+      {locale: I18n.locale}
+    end
+
+    #set language for internationalization
+    def set_locale
+      # Se non ho assegnato il parametro :locale allora gli passo la lingua impostata sul browser
+      # (per testare usa Google chrome Extension: Locale Switcher)
+      params[:locale] = request.env.fetch('HTTP_ACCEPT_LANGUAGE', '').scan(/[a-z]{2}/).first if params[:locale].blank?
+      case params[:locale]
+      when "it", "en"
+        I18n.locale = params[:locale]
+      else
+        I18n.locale = I18n.default_locale
+      end
+    end
 end
