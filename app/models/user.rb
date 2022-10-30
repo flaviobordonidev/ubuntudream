153a90c2b1ba05bb1a425258f6c89c73e2797095
flavio <flavio.bordoni.dev@gmail.com>
2022-10-30 12:46:22 +0100

    Add role:enum to table users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :registerable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable,
          :recoverable, :rememberable, :validatable
-    
+
+  #enum role: [:user, :admin, :moderator, :author]
+  enum role: {user: 0, admin: 1, moderator:2, author:3}
+
   ## ActiveStorage
   has_one_attached :avatar_image
 
